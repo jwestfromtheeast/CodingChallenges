9901a866bc93d9667be35ca9e4ee98f89fe96a55
Justin Westley <justinwestley11@gmail.com>
2018-11-13 15:27:45 -0500

    Update removeElement.cpp

--- removeElement.cpp
+++ removeElement.cpp
@@ -2,9 +2,9 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int count = 0;
-        for(int i = 0; i < nums.size(); i++) {
-            if(nums[i] != val) {
+        int count = 0; //slow
+        for(int i = 0; i < nums.size(); i++) { //fast
+            if(nums[i] != val) { //if this loop is not triggered, it adjusts all future elements back by how many times triggered.
                 nums[count] = nums[i];
                 count++;
             }
