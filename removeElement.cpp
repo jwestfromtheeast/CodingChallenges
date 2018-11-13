03ef1e827065de4b3be95678e927b9881dfcf563
Justin Westley <justinwestley11@gmail.com>
2018-11-13 15:25:30 -0500

    Create removeElement.cpp

---
+++ removeElement.cpp
@@ -0,0 +1,14 @@
+//Leetcode: 27. Remove Element
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int count = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] != val) {
+                nums[count] = nums[i];
+                count++;
+            }
+        }
+        return count;
+    }
+};
