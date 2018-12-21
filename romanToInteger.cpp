27907a4caded8ffa394e00f5f773b4fd0897dc81
Justin Westley <justinwestley11@gmail.com>
2018-12-20 21:47:16 -0500

    Create romanToInteger.cpp

---
+++ romanToInteger.cpp
@@ -0,0 +1,71 @@
+//Leetcode 13. Roman to Integer
+
+class BruteForceSolution {
+public:
+    int romanToInt(string s) {
+        int total = 0;
+        for(int i = 0; i < (int)s.size(); i++){
+            char current = s[i];
+            switch(current) {
+                case 'I':
+                    if(s[i+1] == 'V') {
+                        total += 4;
+                        i ++;
+                        break;
+                    }
+                    else if(s[i+1] == 'X') {
+                        total += 9;
+                        i++;
+                        break;
+                    }
+                    else {
+                        total += 1;
+                        break;
+                    }
+                case 'V':
+                    total += 5;
+                    break;
+                case 'X':
+                     if(s[i+1] == 'L') {
+                        total += 40;
+                        i ++;
+                        break;
+                    }
+                    else if(s[i+1] == 'C') {
+                        total += 90;
+                        i++;
+                        break;
+                    }
+                    else {
+                        total += 10;
+                        break;
+                    }
+                case 'L':
+                    total += 50;
+                    break;
+                case 'C':
+                    if(s[i+1] == 'D') {
+                        total += 400;
+                        i ++;
+                        break;
+                    }
+                    else if(s[i+1] == 'M') {
+                        total += 900;
+                        i++;
+                        break;
+                    }
+                    else {
+                        total += 100;
+                        break;
+                    }
+                case 'D':
+                    total += 500;
+                    break;
+                case 'M':
+                    total += 1000;
+                    break;
+            }
+        }
+        return total;
+    }
+};
