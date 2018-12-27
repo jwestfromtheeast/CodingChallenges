10933dadb98593196d0aa179f66f17c5d5abe5d4
Justin Westley <justinwestley11@gmail.com>
2018-12-27 13:05:31 -0500

    Update romanToInteger.cpp

--- romanToInteger.cpp
+++ romanToInteger.cpp
@@ -69,3 +69,32 @@ public:
         return total;
     }
 };
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        //create map of the possible chars and their integer values
+        unordered_map<char,int> values = { {'I', 1},
+                                           {'V', 5},
+                                           {'X', 10},
+                                           {'L', 50},
+                                           {'C', 100},
+                                           {'D', 500},
+                                           {'M', 1000} };
+        
+        //init the sum, or total, with the last number
+        int sum = values[s.back()];
+        //go from 2nd to last to front. if a value is greater/equal than the next, it can be added, else, subtracted
+        for(int i = s.length() - 2; i >= 0; i--) {
+            if(values[s[i]] >= values[s[i+1]]) {
+                sum += values[s[i]];
+            }
+            else {
+                sum -= values[s[i]];
+            }
+        }
+        
+        return sum;
+        
+    }
+};
