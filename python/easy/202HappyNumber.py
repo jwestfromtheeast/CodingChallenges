b681f0f621e77817c7084ad72e69d4d5803bb2d5
Justin Westley <justinwestley11@gmail.com>
2019-09-23 17:41:38 -0400

    happy number

---
+++ python/easy/202HappyNumber.py
@@ -0,0 +1,26 @@
+class Solution:
+    # Store all previous numbers, so if we see a previous, have hit a loop
+    # Complexity: Time O(n^2) Space O(n)
+    def isHappy(self, n: int) -> bool:
+        prev = set()
+        while n != 1:
+            next_n = 0
+            while n > 0:
+                next_digit = n % 10
+                next_n += (next_digit * next_digit)
+                n //= 10
+            if next_n in prev:
+                return False
+            prev.add(next_n)
+            n = next_n
+        return True
+    
+    def isHappyPythonic(self, n: int) -> bool:
+        prev = set()
+        while n != 1:
+            next_n = sum([int(i) ** 2 for i in str(n)])
+            if next_n in prev:
+                return False
+            prev.add(next_n)
+            n = next_n
+        return True
